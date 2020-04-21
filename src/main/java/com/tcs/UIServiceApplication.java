695587f964f041ed9a82e6be536b6029e1eb4b8b
Sachin Rana <35869428+TheAndroCoder@users.noreply.github.com>
2020-04-21 19:41:20 +0530

    Update UIServiceApplication.java

--- src/main/java/com/tcs/UIServiceApplication.java
+++ src/main/java/com/tcs/UIServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@author Sachin Rana
+
 @SpringBootApplication
 @EnableEurekaClient
 public class UIServiceApplication {
